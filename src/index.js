a352fe627d82459118287a434b665c5eb1faf848
Ndhhiep <hiep.ndh2376@sinhvien.hoasen.edu.vn>
2025-12-28 16:27:34 +0700

    create blog

---
+++ src/index.js
@@ -0,0 +1,31 @@
+const express = require('express')
+const morgan = require('morgan')
+const path = require('path')
+const handlebars = require('express-handlebars')
+
+const app = express()
+const port = 3000
+
+app.use(express.static(path.join(__dirname, 'public')))
+// Http logger
+app.use(morgan('combined'))
+
+// Template engine
+app.set('views', path.join(__dirname, 'resources/views'))
+app.engine('hbs', handlebars.engine({
+    extname: '.hbs',
+}))
+app.set('view engine', 'hbs')
+
+app.get('/', (req, res) => {
+    res.render('home')
+})
+
+app.get('/news', (req, res) => {
+    res.render('news')
+})
+
+
+app.listen(port, () => {
+    console.log(`Example app listening on port http://localhost:${port}`)
+})
